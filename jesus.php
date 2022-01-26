a79e0d125908a15a47da2d75062742868ab4cd2a
Tiago07luiz <tiago26luiz@gmail.com>
2022-01-26 18:48:00 -0500

    subindo os arquivos

---
+++ jesus.php
@@ -0,0 +1,2 @@
+<?php
+ echo "kingdom of heaven";
---
+++ nome_do_seu_arquivo.php
@@ -0,0 +1,16 @@
+<?php
+    echo "studying Php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudando PHP</title>
+</head>
+<body>
+    <h1>Good Look!</h1>
+</body>
+</html>
